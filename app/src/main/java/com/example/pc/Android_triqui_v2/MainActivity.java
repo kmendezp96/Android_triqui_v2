80df886ef1eacb50244d394ec01aac5a2a15f5b9
kmendezp96 <kmendezp@unal.edu.co>
2018-09-06 22:43:15 -0500

    challenge and optional

--- app/src/main/java/com/example/pc/Android_triqui_v2/MainActivity.java
+++ app/src/main/java/com/example/pc/Android_triqui_v2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.pc.Android_triqui_v2;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public static final String HUMAN_PLAYER = "X";
     public static final String COMPUTER_PLAYER = "O";
+    static final int DIALOG_DIFFICULTY_ID = 0;
+    static final int DIALOG_QUIT_ID = 1;
 
     public enum DifficultyLevel {Easy, Harder, Expert};
 
@@ -75,6 +83,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                resetGame();
+                return true;
+            case R.id.ai_difficulty:
+                showDialog(DIALOG_DIFFICULTY_ID);
+                return true;
+            case R.id.quit:
+                showDialog(DIALOG_QUIT_ID);
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        switch (id) {
+            case DIALOG_DIFFICULTY_ID:
+
+                builder.setTitle(R.string.difficulty_choose);
+
+                final CharSequence[] levels = {
+                        getResources().getString(R.string.difficulty_easy),
+                        getResources().getString(R.string.difficulty_harder),
+                        getResources().getString(R.string.difficulty_expert)};
+
+
+                // TODO: Set selected, an integer (0 to n-1), for the Difficulty dialog.
+                // selected is the radio button that should be selected.
+                int selected=0;
+                if (mDifficultyLevel.equals(DifficultyLevel.Easy))
+                    selected = 0;
+                else if (mDifficultyLevel.equals(DifficultyLevel.Harder))
+                    selected = 1;
+                else
+                    selected = 2;
+                builder.setSingleChoiceItems(levels, selected,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int item) {
+                                dialog.dismiss();   // Close dialog
+
+                                // TODO: Set the diff level of mGame based on which item was selected.
+                                if (item==0)
+                                    mDifficultyLevel = DifficultyLevel.Easy;
+                                else if (item==1)
+                                    mDifficultyLevel = DifficultyLevel.Harder;
+                                else
+                                    mDifficultyLevel = DifficultyLevel.Expert;
+                                // Display the selected difficulty level
+                                Toast.makeText(getApplicationContext(), levels[item],
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog = builder.create();
+
+                break;
+
+            case DIALOG_QUIT_ID:
+            // Create the quit confirmation dialog
+                builder.setMessage(R.string.quit_question)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.no, null);
+                dialog = builder.create();
+                break;
+
+        }
+
+        return dialog;
     }
 
     @Override
@@ -209,15 +306,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private int getComputerMove()
-    {
-        int move;
-        Random mRand = new Random();
+    private int getComputerMove() {
+        int move = -1;
         String [] field = new String [9];
         for (int i=0; i<9;i++){
             field[i] = buttons[i].getText().toString();
 
         }
+
+        if (mDifficultyLevel == DifficultyLevel.Easy)
+            move = getRandomMove(field);
+        else if (mDifficultyLevel == DifficultyLevel.Harder) {
+            move = getWinningMove(field);
+            if (move == -1)
+                move = getRandomMove(field);
+        }
+        else if (mDifficultyLevel == DifficultyLevel.Expert) {
+
+            // Try to win, but if that's not possible, block.
+            // If that's not possible, move anywhere.
+            move = getWinningMove(field);
+            if (move == -1)
+                move = getBlockingMove(field);
+            if (move == -1)
+                move = getRandomMove(field);
+        }
+
+        return move;
+
+
+
+    }
+
+    private int getWinningMove(String [] field){
         // First see if there's a move O can make to win
         for (int i = 0; i < 9; i++) {
             if ((!field[i].equals(HUMAN_PLAYER))  && (!field[i].equals(COMPUTER_PLAYER))) {
@@ -231,7 +352,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     field[i] = curr;
             }
         }
+        return -1;
+    }
 
+    private int getBlockingMove(String [] field){
         // See if there's a move O can make to block X from winning
         for (int i = 0; i < 9; i++) {
             if  ((!field[i].equals(HUMAN_PLAYER))  && (!field[i].equals(COMPUTER_PLAYER))){
@@ -245,8 +369,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     field[i] = curr;
             }
         }
+        return -1;
+    }
 
+    private int getRandomMove(String [] field){
         // Generate random move
+        int move = -1;
+        Random mRand = new Random();
         do
         {
             move = mRand.nextInt(9);
@@ -254,6 +383,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return move;
     }
+
 
 
     private void player1Wins(){
